32ddf0a1aa2f60c55d724614e3a2042a27de4fb6
LTSA-Joel <asmx22jperez@inslaferreria.cat>
2025-05-21 17:10:34 +0200

    Update HelloGit.java

--- HelloGit.java
+++ HelloGit.java
@@ -10,7 +10,7 @@ public class HelloGit {
      * @param args the command line arguments
      */
   public static void main(String[] args) {
-    System.out.println("Constructor de Hijo");
+    System.out.println("Print nou d'un col·laborador");
   }
     
 }
