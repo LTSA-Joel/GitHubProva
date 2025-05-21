726ca8db757d5631a0b2da95cb3c068a2566293b
LTSA-Joel <asmx22jperez@inslaferreria.cat>
2025-05-21 17:06:41 +0200

    Create HelloGit.java

---
+++ HelloGit.java
@@ -0,0 +1,16 @@
+package GitHubProva;
+
+/**
+ *
+ * @author famil
+ */
+public class HelloGit {
+
+    /**
+     * @param args the command line arguments
+     */
+  public static void main(String[] args) {
+    System.out.println("Constructor de Hijo");
+  }
+    
+}
